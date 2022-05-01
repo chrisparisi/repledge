652a1d9ada54dca6347031b7ac1c47868048a53c
DingusDad <the.chris.parisi@gmail.com>
2022-05-01 12:54:03 -0700

    minor css changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "../components/navbar/Navbar";
 
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="home">Home</div>
+    </>
+  );
 };
 
 export default Home;
