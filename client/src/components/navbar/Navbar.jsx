e5aca76b7e2e1cac7e0a074feb80c11b246316bf
DingusDad <the.chris.parisi@gmail.com>
2022-05-02 11:37:47 -0700

    minor navbar adjustments

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,7 +3,19 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__left">
+        <h1>Repledge</h1>
+      </div>
+      <div className="navbar__right">
+        <ul>
+          <li>Member Login</li>
+          <li>Get Started Now</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
