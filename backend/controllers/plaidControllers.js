550437a9b774be1714446c6b7e40218aedd1ee9d
DingusDad <the.chris.parisi@gmail.com>
2022-06-18 07:24:31 -0700

    controller and routes reorganization

--- backend/controllers/plaidControllers.js
+++ backend/controllers/plaidControllers.js
@@ -1,1 +1,23 @@
 const asyncHandler = require('express-async-handler');
+
+const plaidClient = new plaid.Client({
+  ClientID: process.env.CLIENT_ID,
+  secret: process.env.SECRET,
+  env: plaid.environments.sandbox,
+});
+
+const createLinkToken = asyncHandler(async (req, res) => {
+  const { link_token: linkToken } = await plaidClient.createLinkToken({
+    user: {
+      client_user_id: 'unique_id',
+    },
+    client_name: 'repledge test',
+    products: ['auth', 'identity'],
+    country_codes: ['US'],
+    language: 'en',
+  });
+
+  res.json({ linkToken });
+});
+
+module.exports = { createLinkToken };
--- backend/routes/plaidRoutes.js
+++ backend/routes/plaidRoutes.js
@@ -1,29 +1,8 @@
 const express = require('express');
 const router = express.Router();
-require('dotenv').config();
 
-const {} = require();
+const { createLinkToken } = require('../controllers/plaidControllers');
 
-const plaidClient = new plaid.Client({
-  ClientID: process.env.CLIENT_ID,
-  secret: process.env.SECRET,
-  env: plaid.environments.sandbox,
-});
-
-app.get('/create-link-token', async (req, res) => {
-  const { link_token: linkToken } = await plaidClient.createLinkToken({
-    user: {
-      client_user_id: 'unique_id',
-    },
-    client_name: 'repledge test',
-    products: ['auth', 'identity'],
-    country_codes: ['US'],
-    language: 'en',
-  });
-
-  res.json({ linkToken });
-});
-
-router.route();
+app.route('/create-link-token').get(createLinkToken);
 
 module.exports = router;
