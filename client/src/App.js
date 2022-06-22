d4c4621ffe219ebd48cb13852f49605392b291ac
DingusDad <the.chris.parisi@gmail.com>
2022-06-22 13:49:50 -0700

    added login and register functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
+import Dashboard from './pages/dashboard/Dashboard';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { FaSignInAlt, FaUser, FaSignOutAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './Navbar.scss';
+import { logout, reset } from '../../features/auth/authSlice';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__left">
-        <h1>Repledge</h1>
+        <Link to="/">Repledge</Link>
       </div>
       <div className="navbar__right">
-        <ul>
-          <Link
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-            to="/login"
-          >
-            Member Login
-          </Link>
-          <li>Get Started Now</li>
-        </ul>
+        {user ? (
+          <ul>
+            <button onClick={onLogout}>Logout</button>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <Link
+                style={{ color: 'inherit', textDecoration: 'inherit' }}
+                to="/login"
+              >
+                Member Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={{ color: 'inherit', textDecoration: 'inherit' }}
+                to="/register"
+              >
+                Get Started
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
